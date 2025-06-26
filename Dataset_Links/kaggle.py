2a77bdee902e8e07bd90bc46c93f5dffbb4397fb
Aryan Saini <aryan2212saini@gmail.com>
2025-06-26 16:01:16 +0530

    Update kaggle.py

--- Dataset_Links/kaggle.py
+++ Dataset_Links/kaggle.py
@@ -1,7 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("abdallamohamed312/in-the-wild-audio-deepfake")
 path = kagglehub.dataset_download("mohammedabdeldayem/the-fake-or-real-dataset")
 
 print("Path to dataset files:", path)
