c489eab5a9bd91fd1ada9c6fde3af73792bba19f
Aryan Saini <aryan2212saini@gmail.com>
2025-08-09 15:22:47 +0530

    Set DEBUG to True and update image model path

--- DeepfakeDetector/DeepfakeDetector/settings.py
+++ DeepfakeDetector/DeepfakeDetector/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j&ss*)*+s)xur(8kw-by=-o*diw_2gz)bkw^2zu9_f=75dvimo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -131,4 +131,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEBUG = False
+DEBUG = True
